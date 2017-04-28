68701881e2848e3f4ded7104b32532187099a534
forstmeier <john.forstmeier@gmail.com>
2017-04-27 21:16:37 -0400

    Another progress commit
    Description:
    Just for maintaining my current position w/o losing anything.
    
    Build well

--- engine/retriever/query.go
+++ engine/retriever/query.go
@@ -47,6 +47,7 @@ const ISSUE_QUERY = `
         LEFT JOIN reactions ON issues.id=reactions.issue_fk
     WHERE (id > ?);
 `
+// Query: All values for current date starting from current time - 500ms
 
 // TODO: Build to support a PR query as well.
 func (d *Database) Read() ([]*github.Issue, error) {
@@ -57,17 +58,87 @@ func (d *Database) Read() ([]*github.Issue, error) {
     defer results.Close()
 
     id := 0
-    issues := []*github.Issue{} // NOTE: Final output slice.
+    issues := []*github.Issue{}
     issue := new(github.Issue)
+    userID := sql.NullInt64
 
     for results.Next() {
-        if err := results.Scan(&id, &issue.ID, &issue.Number, &issue.State, &issue.Locked, &issue.Title, &issue.Body, &issue.Comments, &issue.ClosedAt, &issue.CreatedAt, &issue.UpdatedAt, &issue.URL, &issue.HTMLURL,
-                                // THE REST OF THE SCAN GO HERE
+        if err := results.Scan(&id, &issue.ID, &issue.Number, &issue.State, &issue.Locked, // NOTE: Issue struct from MemSQL
+                                    &issue.Title, &issue.Body, &issue.Comments, &issue.ClosedAt,
+                                    &issue.CreatedAt, &issue.UpdatedAt, &issue.URL, &issue.HTMLURL,
+                                &userID, &issue.User.ID, &issue.User.AvatarURL, &issue.User.HTMLURL, // NOTE: User field on Issue struct
+                                    &issue.User.GravatarID, &issue.User.Name, &issue.User.Company,
+                                    &issue.User.Blog, &issue.User.Location, &issue.User.Email,
+                                    &issue.User.Hireable, &issue.User.Bio, &issue.User.PublicRepos,
+                                    &issue.User.PublicGists, &issue.User.Followers, &issue.User.Following,
+                                    &issue.User.CreatedAt, &issue.User.UpdatedAt, &issue.User.SuspendedAt,
+                                    &issue.User.Type, &issue.User.SiteAdmin, &issue.User.TotalPrivateRepos,
+                                    &issue.User.OwnedPrivateRepos, &issue.User.PrivateGists,
+                                    &issue.User.DiskUsage, &issue.User.Collaborators,
+                                    &issue.User.Plan.Name, &issue.User.Plan.Spance, // NOTE: Issue User Plan info
+                                    &issue.User.Plan.Collaborators, &issue.User.Plan.PrivateRepos,
+                                    &issue.User.URL, &issue.User.EventsURL, &issue.User.FollowingURL,
+                                    &issue.User.FollowersURL, &issue.User.GistsURL, &issue.User.OrganizationsURL,
+                                    &issue.User.ReceivedEventsURL, &issue.User.ReposURL, &issue.User.StarredURL,
+                                    &issue.User.SubscriptionsURL,
+                                &userID, &issue.Assignee.ID, &issue.Assignee.AvatarURL, &issue.Assignee.HTMLURL, // NOTE: Assignee field on Issue struct
+                                    &issue.Assignee.GravatarID, &issue.Assignee.Name, &issue.Assignee.Company,
+                                    &issue.Assignee.Blog, &issue.Assignee.Location, &issue.Assignee.Email,
+                                    &issue.Assignee.Hireable, &issue.Assignee.Bio, &issue.Assignee.PublicRepos,
+                                    &issue.Assignee.PublicGists, &issue.Assignee.Followers, &issue.Assignee.Following,
+                                    &issue.Assignee.CreatedAt, &issue.Assignee.UpdatedAt, &issue.Assignee.SuspendedAt,
+                                    &issue.Assignee.Type, &issue.Assignee.SiteAdmin, &issue.Assignee.TotalPrivateRepos,
+                                    &issue.Assignee.OwnedPrivateRepos, &issue.Assignee.PrivateGists,
+                                    &issue.Assignee.DiskUsage, &issue.Assignee.Collaborators,
+                                    &issue.Assignee.Plan.Name, &issue.Assignee.Plan.Spance, // NOTE: Issue Assignee Plan info
+                                    &issue.Assignee.Plan.Collaborators, &issue.Assignee.Plan.PrivateRepos,
+                                    &issue.Assignee.URL, &issue.Assignee.EventsURL, &issue.Assignee.FollowingURL,
+                                    &issue.Assignee.FollowersURL, &issue.Assignee.GistsURL, &issue.Assignee.OrganizationsURL,
+                                    &issue.Assignee.ReceivedEventsURL, &issue.Assignee.ReposURL, &issue.Assignee.StarredURL,
+                                    &issue.Assignee.SubscriptionsURL,
+                                &issue.Comments, &issue.ClosedAt, &issue.CreatedAt, &issue.UpdatedAt, // NOTE: General fields on User
+                                &userID, &issue.ClosedBy.ID, &issue.ClosedBy.AvatarURL, &issue.ClosedBy.HTMLURL, // NOTE: ClosedBy field on Issue struct
+                                    &issue.ClosedBy.GravatarID, &issue.ClosedBy.Name, &issue.ClosedBy.Company,
+                                    &issue.ClosedBy.Blog, &issue.ClosedBy.Location, &issue.ClosedBy.Email,
+                                    &issue.ClosedBy.Hireable, &issue.ClosedBy.Bio, &issue.ClosedBy.PublicRepos,
+                                    &issue.ClosedBy.PublicGists, &issue.ClosedBy.Followers, &issue.ClosedBy.Following,
+                                    &issue.ClosedBy.CreatedAt, &issue.ClosedBy.UpdatedAt, &issue.ClosedBy.SuspendedAt,
+                                    &issue.ClosedBy.Type, &issue.ClosedBy.SiteAdmin, &issue.ClosedBy.TotalPrivateRepos,
+                                    &issue.ClosedBy.OwnedPrivateRepos, &issue.ClosedBy.PrivateGists,
+                                    &issue.ClosedBy.DiskUsage, &issue.ClosedBy.Collaborators,
+                                    &issue.ClosedBy.Plan.Name, &issue.ClosedBy.Plan.Spance, // NOTE: Issue ClosedBy Plan info
+                                    &issue.ClosedBy.Plan.Collaborators, &issue.ClosedBy.Plan.PrivateRepos,
+                                    &issue.ClosedBy.URL, &issue.ClosedBy.EventsURL, &issue.ClosedBy.FollowingURL,
+                                    &issue.ClosedBy.FollowersURL, &issue.ClosedBy.GistsURL, &issue.ClosedBy.OrganizationsURL,
+                                    &issue.ClosedBy.ReceivedEventsURL, &issue.ClosedBy.ReposURL, &issue.ClosedBy.StarredURL,
+                                    &issue.ClosedBy.SubscriptionsURL
+                                &issue.URL, &issue.HTMLURL, // NOTE: Link fields for Issue struct
+                                &issue.Milestone.URL, &issue.Milestone.HTMLURL, &issue.Milestone.LabelsURL, // NOTE: Milestone field on Issue
+                                    &issue.Milestone.ID, &issue.Milestone.Number, &issue.Milestone.State,
+                                    &issue.Milestone.Title, &issue.Milestone.Description,
+                                    &userID, &issue.Milestone.Creator.ID, &issue.Milestone.Creator.AvatarURL, &issue.Milestone.Creator.HTMLURL, // NOTE: Creator field on Milestone struct
+                                        &issue.Milestone.Creator.GravatarID, &issue.Milestone.Creator.Name, &issue.Milestone.Creator.Company,
+                                        &issue.Milestone.Creator.Blog, &issue.Milestone.Creator.Location, &issue.Milestone.Creator.Email,
+                                        &issue.Milestone.Creator.Hireable, &issue.Milestone.Creator.Bio, &issue.Milestone.Creator.PublicRepos,
+                                        &issue.Milestone.Creator.PublicGists, &issue.Milestone.Creator.Followers, &issue.Milestone.Creator.Following,
+                                        &issue.Milestone.Creator.CreatedAt, &issue.Milestone.Creator.UpdatedAt, &issue.Milestone.Creator.SuspendedAt,
+                                        &issue.Milestone.Creator.Type, &issue.Milestone.Creator.SiteAdmin, &issue.Milestone.Creator.TotalPrivateRepos,
+                                        &issue.Milestone.Creator.OwnedPrivateRepos, &issue.Milestone.Creator.PrivateGists,
+                                        &issue.Milestone.Creator.DiskUsage, &issue.Milestone.Creator.Collaborators,
+                                        &issue.Milestone.Creator.Plan.Name, &issue.Milestone.Creator.Plan.Spance, // NOTE: Milestone Creator Plan info
+                                        &issue.Milestone.Creator.Plan.Collaborators, &issue.Milestone.Creator.Plan.PrivateRepos,
+                                        &issue.Milestone.Creator.URL, &issue.Milestone.Creator.EventsURL, &issue.Milestone.Creator.FollowingURL,
+                                        &issue.Milestone.Creator.FollowersURL, &issue.Milestone.Creator.GistsURL, &issue.Milestone.Creator.OrganizationsURL,
+                                        &issue.Milestone.Creator.ReceivedEventsURL, &issue.Milestone.Creator.ReposURL, &issue.Milestone.Creator.StarredURL,
+                                        &issue.Milestone.Creator.SubscriptionsURL,
+                                    &issue.Milestone.OpenIssues, &issue.Milestone.ClosedIssues, &issue.Milestone.CreatedAt,
+                                    &issue.Milestone.UpdatedAt, &issue.Milestone.ClosedAt, &issue.Milestone.DueOn,
+
+
         ); err != nil {
             return nil, err
         }
-    //     BUILD FULL ISSUE HERE AND THEN APPEND TO OUTPUT LIST
-    //     issues = append(issues, issue)
+        issues = append(issues, issue)
         issueID = *issue.ID
     }
 
@@ -76,13 +147,6 @@ func (d *Database) Read() ([]*github.Issue, error) {
     // For each Issue in the Issues Slice
     //    Lookup Assignee ID and Populate Assignee Struct on the Issue struct
     //    Lookup Label ID and Populate Labels Struct on the Issue struct
-
-
-    // QUERY: (ALL PRIOR STUFF) AND WHERE > LAST ID
-    // ^ LAST ID = 0 on startup
-    // ^ Set LAST ID = max(id)
-
-    // Query: All values for current date starting from current time - 500ms
 
     // Reads newest values in db (since last call)
     // Populates results into struct
