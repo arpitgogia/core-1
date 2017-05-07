5e45c5529cffcd3a1502fbb9d63efc3476658e24
forstmeier <john.forstmeier@gmail.com>
2017-05-07 17:53:49 -0400

    Write initial unit tests
    Description:
    This is not a full unit test and I think we should consider having the
    NewWorker function generate a Worker pointer for a more thorough evaluation
    of the operation (e.g nil comparison).
    
    Build well

--- engine/retriever/worker_test.go
+++ engine/retriever/worker_test.go
@@ -1,1 +1,16 @@
 package retriever
+
+import (
+	"testing"
+
+	"github.com/google/go-github/github"
+)
+
+func TestNewWorker(t *testing.T) {
+	testID := 1
+	channel := make(chan chan github.Issue)
+	testWorker := NewWorker(testID, channel)
+	if testWorker.ID != testID {
+		t.Error("Failure creating new worker object")
+	}
+}
