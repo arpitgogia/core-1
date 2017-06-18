23b5fdaee96e0ead3e29b8ff4e7bf1ff7743a075
forstmeier <john.forstmeier@gmail.com>
2017-06-18 18:19:21 -0400

    Rebuild storage functionality
    Description:
    The in-memory storage has been rebuilt into two buckets within
    the storage.db file which holds only repoID keys.
    
    Note: All unit tests are passing.
    Note: Unit tests are non-comprehensive.
    Note: All storage methods are technically test covered.
    Note: Most method calls have been implemented correctly.
    Note: Some subsequent query results have not been integrated.
    
    Build well

--- engine/frontend/storage.go
+++ engine/frontend/storage.go
@@ -1,4 +1,4 @@
-package signup
+package frontend
 
 import (
 	"fmt"
@@ -7,35 +7,21 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-// DOC: Keys thus far are "hookID" and "token" in buckets named "repoID".
+// DOC: Buckets thus far are "hook" and "token" with keys named "repoID".
 type BoltDB struct {
-	db *bolt.DB
+	DB *bolt.DB
 }
 
-func (b *BoltDB) store(repoID int, key string, value interface{}) error {
-	idBytes := []byte(strconv.Itoa(repoID))
-	keyBytes := []byte(key)
-	valueBytes, err := func(input interface{}) ([]byte, error) {
-		switch i := input.(type) {
-		case int:
-			return []byte(strconv.Itoa(i)), nil
-		case string:
-			return []byte(i), nil
-		default:
-			return nil, fmt.Errorf("Invalid input type %v; accepted types: int, string", i)
-		}
-	}(value)
-	if err != nil {
-		return err
-	}
-
-	err = b.db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists(idBytes)
+func (b *BoltDB) Store(name string, key int, value []byte) error {
+	byteName := []byte(name)
+	byteKey := []byte(strconv.Itoa(key))
+	err := b.DB.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists(byteName)
 		if err != nil {
 			return err
 		}
 
-		err = bucket.Put(keyBytes, valueBytes)
+		err = bucket.Put(byteKey, value)
 		if err != nil {
 			return err
 		}
@@ -47,70 +33,57 @@ func (b *BoltDB) store(repoID int, key string, value interface{}) error {
 	return nil
 }
 
-func (b *BoltDB) retrieve(repoID int, key string) (interface{}, error) {
-	idBytes := []byte(strconv.Itoa(repoID))
-	keyBytes := []byte(key)
-	valueBytes := []byte{}
+func (b *BoltDB) Retrieve(name string, key int) ([]byte, error) {
+	byteName := []byte(name)
+	byteKey := []byte(strconv.Itoa(key))
+	byteValue := []byte{}
 
-	err := b.db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket(idBytes)
+	err := b.DB.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(byteName)
 		if bucket == nil {
-			return fmt.Errorf("Repository %v not found in database", repoID)
+			return fmt.Errorf("Bucket %v not found in database", name)
 		}
-		valueBytes = bucket.Get(keyBytes)
-		if valueBytes == nil {
-			return fmt.Errorf("Key %v not found in database for repository %v", key, repoID)
+		byteValue = bucket.Get(byteKey)
+		if byteValue == nil {
+			return fmt.Errorf("Repo %v not found in database for bucket %v", key, name)
 		}
 		return nil
 	})
 	if err != nil {
 		return nil, err
 	}
-
-	result, err := func(input []byte) (interface{}, error) {
-		if num, err := strconv.ParseUint(string(input), 10, 8); err == nil {
-			return int(num), nil
-		} else {
-			return string(input), nil
-		}
-	}(valueBytes)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+	return byteValue, nil
 }
 
-func (b *BoltDB) retrieveBulk(input ...string) ([][]byte, error) {
-	tokens := [][]byte{}
-	if err := b.db.Batch(func(tx *bolt.Tx) error {
-		if err := tx.ForEach(func(name []byte, b *bolt.Bucket) error {
-			bucket := tx.Bucket(name)
-			tokens = append(tokens, bucket.Get([]byte("token")))
+func (b *BoltDB) RetrieveBulk(name string) ([][]byte, [][]byte, error) {
+	keys := [][]byte{}
+	values := [][]byte{}
+
+	if err := b.DB.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(name))
+		bucket.ForEach(func(key, value []byte) error {
+			keys = append(keys, key)
+			values = append(values, value)
 			return nil
-		}); err != nil {
+		})
+		return nil
+	}); err != nil {
+		return nil, nil, err
+	}
+	return keys, values, nil
+}
+
+func (b *BoltDB) Delete(name string, key int) error {
+	byteName := []byte(name)
+	byteKey := []byte(strconv.Itoa(key))
+
+	if err := b.DB.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(byteName)
+		if err := bucket.Delete(byteKey); err != nil {
 			return err
 		}
 		return nil
 	}); err != nil {
-		return nil, err
-	}
-	return tokens, nil
-}
-
-// DOC: For when users remove their repos from the Heupr service.
-func (b *BoltDB) delete(repoID int) error {
-	idBytes := []byte(strconv.Itoa(repoID))
-
-	err := b.db.Update(func(tx *bolt.Tx) error {
-		if err := tx.DeleteBucket(idBytes); err != nil {
-			return err
-		}
-		if tx.Bucket(idBytes) != nil {
-			return fmt.Errorf("Target repo %v not removed", repoID)
-		}
-		return nil
-	})
-	if err != nil {
 		return err
 	}
 	return nil
--- engine/frontend/storage_test.go
+++ engine/frontend/storage_test.go
@@ -1,42 +1,39 @@
-package signup
+package frontend
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func Test_open(t *testing.T) {
 	testServer := FrontendServer{}
-	testBucket := 2187
-	testKey := "Leia"
-	testValue := "Princess of Alderaan"
+	testBucket := "hook"
+	testKey := 7
+	testValue := []byte(strconv.Itoa(2224))
 
 	defer testServer.CloseBolt()
-	err := testServer.OpenBolt()
-	if err != nil {
-		t.Errorf("Error opening test database: ", err)
-	}
-
-	if err != nil {
+	if err := testServer.OpenBolt(); err != nil {
 		t.Errorf("Error opening new database instance: %v", err)
 	}
 	t.Run("store", func(t *testing.T) {
-		err := testServer.Database.store(testBucket, testKey, testValue)
-		if err != nil {
-			t.Error("Error in adding data to database file")
+		if err := testServer.Database.Store(testBucket, testKey, testValue); err != nil {
+			t.Errorf("Error in adding data to database file: %v", err)
 		}
 	})
 	t.Run("retrieve", func(t *testing.T) {
-		value, err := testServer.Database.retrieve(testBucket, testKey)
+		value, err := testServer.Database.Retrieve(testBucket, testKey)
 		if err != nil {
 			t.Errorf("Error retrieving data from database - expected %v; received %v", testValue, value)
 		}
 	})
 	t.Run("bulk", func(t *testing.T) {
-		_, err := testServer.Database.retrieveBulk()
+		_, _, err := testServer.Database.RetrieveBulk(testBucket)
 		if err != nil {
-			t.Errorf("Error pulling all data from default database: %v", err)
+			t.Errorf("Error pulling all data from bucket: %v", err)
 		}
 	})
 	t.Run("delete", func(t *testing.T) {
-		err := testServer.Database.delete(testBucket)
+		err := testServer.Database.Delete(testBucket, testKey)
 		if err != nil {
 			t.Errorf("Error deleting database entry: %v", err)
 		}
