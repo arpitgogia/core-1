dc26459e7b6830db2207dfca62fc4064de8d43e7
forstmeier <john.forstmeier@gmail.com>
2017-06-28 21:19:11 -0400

    Reimplement ArchRepo assets and logic
    Description:
    Initialization methods for the ArchRepo sub/structs have been repaired
    and improved. Several of the future feature fields have been removed
    and put into a dedicated GitHub Issue for reference.
    
    Some of the logic available in the initialization methods will eventually
    be replaced by more dynamic alternatives (planned feature).
    
    Two unit tests have been provided.
    
    Note: Unit tests are passing.
    Note: Unit tests are not comprehensive (in my opinion).

--- engine/backend/arch_repo.go
+++ engine/backend/arch_repo.go
@@ -3,9 +3,7 @@ package backend
 import (
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-	ghoa "golang.org/x/oauth2/github"
 
-	// "coralreefci/engine/frontend"
 	"coralreefci/engine/gateway/conflation"
 	"coralreefci/models"
 )
@@ -13,29 +11,17 @@ import (
 type ArchModel struct {
 	Model     *models.Model
 	Conflator *conflation.Conflator
-	// Benchmark        Benchmark // TODO: Struct to build
-	// Scenarios        []conflation.Scenario
-	// PilotScenarios   []conflation.Scenario
-	// LearnedScenarios []conflation.Scenario
-	// StrategyParams   StrategyParams TODO: Baseline with self evolving
-	//                                       parameters: Tossing Graph?,
-	//                                       Conflation Scenarios, etc.)
 }
 
 type Blender struct {
 	Models []*ArchModel
-	// PilotModels []*ArchModel
 }
 
 type ArchHive struct {
 	Blender *Blender
-	// TossingGraph       TossingGraphAlgorithm // TODO: Struct to build
-	// StrategyParams     StrategyParams // TODO: Struct to build
-	// AggregateBenchmark Benchmark
 }
 
 type ArchRepo struct {
-	Repo   *github.Repository // NOTE: This is a deprediated field; remove.
 	Hive   *ArchHive
 	Client *github.Client
 }
@@ -43,36 +29,39 @@ type ArchRepo struct {
 func (bs *BackendServer) NewArchRepo(repoID int) {
 	bs.Repos.Lock()
 	defer bs.Repos.Unlock()
-	bs.Repos.Actives[repoID] = &ArchRepo{Hive: &ArchHive{Blender: &Blender{}}}
+
+	ctx := &conflation.Context{}
+	scn := []conflation.Scenario{&conflation.Scenario3{}}
+	algo := []conflation.ConflationAlgorithm{
+		&conflation.ComboAlgorithm{
+			Context: ctx,
+		},
+	}
+	norm := conflation.Normalizer{Context: ctx}
+	conf := conflation.Conflator{
+		Scenarios:            scn,
+		ConflationAlgorithms: algo,
+		Normalizer:           norm,
+		Context:              ctx,
+	}
+	model := ArchModel{Conflator: &conf}
+
+	bs.Repos.Actives[repoID] = &ArchRepo{
+		Hive: &ArchHive{
+			Blender: &Blender{
+				Models: []*ArchModel{&model},
+			},
+		},
+	}
 }
 
 func (bs *BackendServer) NewClient(repoID int, token *oauth2.Token) {
 	bs.Repos.Lock()
 	defer bs.Repos.Unlock()
 
-	oaConfig := &oauth2.Config{
-		ClientID:     "",
-		ClientSecret: "",
-		Endpoint:     ghoa.Endpoint,
-		Scopes:       []string{"admin:repo_hook", "repo:status", "public_repo"}, // NOTE: Scopes may be reduced (e.g. remove hook).
-	}
+	tokenSource := oauth2.StaticTokenSource(token)
+	authClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
+	githubClient := github.NewClient(authClient)
 
-	oaClient := oaConfig.Client(oauth2.NoContext, token)
-	client := github.NewClient(oaClient)
-	bs.Repos.Actives[repoID].Client = client
+	bs.Repos.Actives[repoID].Client = githubClient
 }
-
-// TODO: Instantiate the Conflator struct on the ArchRepo.
-
-// TODO:
-// Below are several potential helper methods for the ArchRepo:
-// BootstrapModel() - performs preliminary training / assignments / startup
-// GetModelBenchmark() TODO: Calculate AggregateBenchmark for this method
-// Assign(issue github.Issue) - assign newly raised issue to contributor
-// Enable()
-// Disable()
-// Destroy()
-// InactiveDevelopers() []string
-// Even though the Github API will prevent it. Heupr could try to assign an
-// issue to an inactive developer. It might not be great for team morale if
-// Heupr inadvertently exposed that in a UI/dashboard page.
