9b72de1f7ca828a9a9c37fc891424b2e545671ce
forstmeier <john.forstmeier@gmail.com>
2017-08-02 22:13:52 -0400

    Implement ingestor restart method
    Because there may be interruptions to the ingestor server, a "restart" method was needed to update the database with information that may have been missed while the server was offline. Because models will likely eventually account for more than just issues/pulls, this method is subject to change. A note has been made in the IssueListByRepoOptions regarding the use of the "Since" option that could possibly be a fine tuning.
    
    Note: Code is building correctly.
    Note: Unit tests have not been written for this file.
    Note: Formatting has been applied.

--- engine/ingestor/server.go
+++ engine/ingestor/server.go
@@ -2,9 +2,11 @@ package ingestor
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
-	// "strconv"
+	"strconv"
 
+	"github.com/boltdb/bolt"
 	"github.com/google/go-github/github"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
@@ -106,9 +108,103 @@ func (i *IngestorServer) Start() {
 	}
 }
 
-func (i *IngestorServer) Restart() {}
+func (i *IngestorServer) Restart() error {
+	db, err := bolt.Open("frontend/storage.db", 0644, nil)
+	if err != nil {
+		utils.AppLog.Error("failed opening bolt on ingestor restart", zap.Error(err))
+		return err
+	}
+	defer db.Close()
 
-func (i *IngestorServer) ContinuityCheck() {}
+	boltDB := frontend.BoltDB{DB: db}
+
+	repos, tokens, err := boltDB.RetrieveBulk("token")
+	if err != nil {
+		utils.AppLog.Error("retrieve bulk tokens on ingestor restart", zap.Error(err))
+	}
+
+	allIssues := []*github.Issue{}
+
+	for i := range tokens {
+		token := oauth2.Token{}
+		if err := json.Unmarshal(tokens[i], &token); err != nil {
+			utils.AppLog.Error("converting tokens; ", zap.Error(err))
+			return err
+		}
+
+		source := oauth2.StaticTokenSource(&token)
+		oaClient := oauth2.NewClient(oauth2.NoContext, source)
+		client := *github.NewClient(oaClient)
+
+		repoID, err := strconv.Atoi(string(repos[i]))
+		if err != nil {
+			utils.AppLog.Error("repo id int conversion; ", zap.Error(err))
+			return err
+		}
+
+		repo, _, err := client.Repositories.GetByID(context.Background(), repoID)
+		if err != nil {
+			utils.AppLog.Error("ingestor restart get by id; ", zap.Error(err))
+			return err
+		}
+
+		owner := repo.Owner.Login
+		name := repo.Name
+		opts := github.IssueListByRepoOptions{
+			State: "open",
+			// Since: time.Time    // may be helpful if a "crash time" can be catalogued
+			ListOptions: github.ListOptions{
+				PerPage: 100,
+			},
+		}
+
+		issues := []*github.Issue{}
+		for {
+			gotIssues, resp, err := client.Issues.ListByRepo(context.Background(), *owner, *name, &opts)
+			if err != nil {
+				utils.AppLog.Error("restart issues pull down; ", zap.Error(err))
+				return err
+			}
+			issues = append(issues, gotIssues...)
+			if resp.NextPage == 0 {
+				break
+			} else {
+				opts.ListOptions.Page = resp.NextPage
+			}
+		}
+
+		allIssues = append(allIssues, issues...)
+
+	}
+
+	bufferPool := NewPool()
+	memSQL := Database{BufferPool: bufferPool}
+	memSQL.Open()
+	defer memSQL.Close()
+
+	memSQL.BulkInsertIssues(allIssues)
+
+	return nil
+}
+
+func (i *IngestorServer) ContinuityCheck() {
+	bufferPool := NewPool()
+	db := Database{BufferPool: bufferPool}
+	db.Open()
+
+	// starts up
+	// runs perpetually
+	// queries on longer:
+	// - time expires
+	// - goroutine complete
+	// query:
+	// - all repos
+	// - - issues of repos
+	// - - pulls of repos
+	// if gaps in issues/pulls IDs
+	// - query GitHub w/ repo info
+	// - update database w/ insert
+}
 
 func (i *IngestorServer) Stop() {
 	//TODO: Closing the server down is a needed operation that will be added.
